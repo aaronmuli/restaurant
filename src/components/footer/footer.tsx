bbd913c9a0d4b2f9d5aea8135971fc6635562e14
aaronmuli <zionmurlley@gmail.com>
2024-11-27 08:38:17 -0800

    added map styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -32,12 +32,16 @@ const Footer = () => {
           <GrLocation size={20} color='#fff' />
           <p>La Piazzetta location</p>
         </div>
-        <iframe 
+        {/* <iframe 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.274257380938!2d-70.56068388481569!3d41.45496659976631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e52963ac45bbcb%3A0xf05e8d125e82af10!2sDos%20Mas!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus" 
           width="400" 
           height="250" 
           loading="lazy">
-          </iframe>
+          </iframe> */}
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3846.5005735775244!2d28.312785410093305!3d-15.40351098512177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19408baf5c4c5299%3A0xaa838b2bd52f3b83!2sLa%20Piazzetta!5e0!3m2!1sen!2szm!4v1732724783462!5m2!1sen!2szm" 
+          width="400" 
+          height="250"   
+          loading="lazy" ></iframe>
       </div>
     </footer>
   )
